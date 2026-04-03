43645d004300488dd0361a9b9ca42801002c7ae5
Tarik Jeait <69420323+tarik-jeait@users.noreply.github.com>
2026-04-03 15:30:26 +0100

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
 namespace MCarBot {
     //% block = "RGB Led Left On $red $green $blue"
     //% red.defl=255
@@ -7,20 +15,45 @@ namespace MCarBot {
     //% blue.defl=255
     //% blue.min=0 blue.max=255
     export function RgbLeftLedOn(red: number, green: number, blue: number) {
-        sendI2cWriteCommand(13, 4, red, green, blue, 0, 0, 0,0);
+        sendI2cWriteCommand(13, 4, red, green, blue, 0, 0, 0);
     }
     //% block = "RGB Led Left Off"
     export function RgbLeftLedOff() {
-        sendI2cWriteCommand(14, 4, 0, 0, 0, 0, 0, 0,0);
+        sendI2cWriteCommand(14, 4, 0, 0, 0, 0, 0, 0);
+    }
+    //% block = "RGB Led Right On $red $green $blue"
+    //% red.defl=255
+    //% red.min=0 red.max=255
+    //% green.defl=255
+    //% green.min=0 green.max=255
+    //% blue.defl=255
+    //% blue.min=0 blue.max=255
+    export function RgbRightLedOn(red: number, green: number, blue: number) {
+        sendI2cWriteCommand(15, 4, red, green, blue, 0, 0, 0);
+    }
+    //% block = "RGB Led Right Off"
+    export function RgbRightLedOff() {
+        sendI2cWriteCommand(16, 4, 0, 0, 0, 0, 0, 0);
+    }
+    //% block = "MCarbot Init"
+    export function McarBotInit() {
+        sendI2cWriteCommand(16, 4, 0, 0, 0, 0, 0, 0);
+    }
+    //% block = "Line Follower Sensors On"
+    export function LfSensorsOn() {
+        sendI2cWriteCommand(24, 4, 0, 0, 0, 0, 0, 0);
+    }
+    //% block = "Line Follower Sensors Off"
+    export function LfSensorsOff() {
+        sendI2cWriteCommand(25, 4, 0, 0, 0, 0, 0, 0);
     }
     // note that Caml casing yields lower case
     // block text with spaces
-
     export function getRpAddress() {
         let rp_address = 0x13;
         return rp_address;
     }
-    function sendI2cWriteCommand(commandId: number, peripheralId: number, arg1: number, arg2: number, arg3: number, arg4: number, arg5: number, arg6: number, arg7: number) {
+    function sendI2cWriteCommand(commandId: number, peripheralId: number, arg1: number, arg2: number, arg3: number, arg4: number, arg5: number, arg6: number) {
         let writeCommandBuffer = pins.createBuffer(9);
         writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 0, commandId);
         writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 1, peripheralId);
@@ -30,7 +63,7 @@ namespace MCarBot {
         writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 5, arg3);
         writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 6, arg4);
         writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 7, arg5);
-        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 8, arg5);
+        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 8, arg6);
         pins.i2cWriteBuffer(getRpAddress(), writeCommandBuffer, false);
     }
 }
