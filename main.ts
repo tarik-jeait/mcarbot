5ebb608ac7a8a4b9cba69b24f016a455eb6fd85e
Tarik Jeait <69420323+tarik-jeait@users.noreply.github.com>
2026-04-03 15:39:47 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,3 @@
-/**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
- */
-
-//% color="#AA278D" weight=100
 namespace MCarBot {
     //% block = "RGB Led Left On $red $green $blue"
     //% red.defl=255
